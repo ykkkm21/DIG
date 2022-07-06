2923a12d841868c050ead663708998c515f2e559
ykkkm21 <105718962+ykkkm21@users.noreply.github.com>
2022-07-06 22:33:06 +0900

    Create slide.js

---
+++ slide.js
@@ -0,0 +1,12 @@
+//画像を配列に格納
+     var images =['url(画像1のURL)',
+                  'url(画像2のURL)',
+                  'url(画像3のURL)']
+ 
+     //要素をHTMLから取得
+     //画像
+     var target = document.getElementById('slide');
+     
+     //>,<
+     var right = document.getElementById('right');
+     var left = document.getElementById('left');
